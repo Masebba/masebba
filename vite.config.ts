305bc0ba8456aa8a23b13de28498f96d72ac8938
Masebba <65603553+Masebba@users.noreply.github.com>
2026-04-09 19:01:17 +0600

    Resolved changes at main repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/",
 });
-=======
-  base: '/',
-})
->>>>>>> 9d116a69ad06e1da4fa0d79608ee0c25ccd80e4b
